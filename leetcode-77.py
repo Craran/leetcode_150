777c68d74c69e647afb9c93d6be5e7d01f04bea0
craran <graydonaldti@gmail.com>
2024-08-06 17:35:45 +0800

    leetcode-77.py-un

--- leetcode-77.py
+++ leetcode-77.py
@@ -1,9 +1,25 @@
 from typing import List
+from collections import deque
 
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         visited = set()
+        tmp = []
+        ans = []
 
+        def dfs(n: int, k: int, x: int, cnt: int) -> None:
+            if cnt >= k:
+                tmp.append(k)
+                ans.append(tmp)
+                return
+            
+            for i in range(1, n + 1):
+                if visited[i] != True and ...:
+                    visited[i] = True
+                    tmp.append(i)
+                    dfs(n, k, i, cnt + 1)
+                    tmp.pop()
+                    visited[i] = False
+                
 
-        def dfs(i: int) -> None:
-            if 
+            
