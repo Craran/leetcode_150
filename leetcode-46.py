5b5dfee4098fd153afe8b87078ebc808b9421bdf
craran <graydonaldti@gmail.com>
2024-08-08 17:16:44 +0800

    finished leetcode-46.py

--- leetcode-46.py
+++ leetcode-46.py
@@ -1,2 +1,30 @@
+from typing import List
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        tmp = []
+        visited = [False for _ in range(len(nums) + 5)]
+
+        def dfs(x: int, cnt: int) -> None:
+            if cnt >= len(nums):
+                result.append(tmp[:])
+                return None
+            
+            for i in range(len(nums)):
+                if visited[i] == True:
+                    continue
+                visited[i] = True
+                tmp.append(nums[i])
+                dfs(i, cnt + 1)
+                visited[i] = False
+                tmp.pop()
+            return None
+        
+        dfs(len(nums), 0)
+        return result
+
+            
+"""
+同样要注意深浅拷贝的问题
+"""
