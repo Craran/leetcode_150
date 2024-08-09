8ff40e95f0b0209d734009ef55d609a942501dc3
craran <graydonaldti@gmail.com>
2024-08-09 18:12:48 +0800

    finished leetcode-69.py

--- leetcode-69.py
+++ leetcode-69.py
@@ -1,2 +1,9 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        x_0 = 1.0
+        x_n = x_0 + 0.0
+        while True:
+            x_np1 = x_n - (x_n * x_n - x) / (2 * x_n)
+            if abs(x_np1 - x_n) < 1e-3:
+                return int(x_np1)
+            x_n = x_np1
