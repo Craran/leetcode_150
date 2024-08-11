0d2f9607a873f4f7f07734f507d02344c86d6cb1
craran <graydonaldti@gmail.com>
2024-08-11 14:10:30 +0800

    update leetcode-209.py-un

--- leetcode-209.py
+++ leetcode-209.py
@@ -1,3 +1,22 @@
 from typing import List
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        sum = nums[0]
+        if sum >= target:
+            return 1
+        i = j = 0
+        n = len(nums)
+        result = 0x7f7f7f7f
+
+        while j < n:
+            while i < j and sum >= target:
+                sum -= nums[i]
+                i += 1
+                
+            while i <= j and sum < target and j < n:
+                j += 1
+                sum += nums[j]
+            
+            if sum >= target:
+                result = min(result, j - i + )
+
