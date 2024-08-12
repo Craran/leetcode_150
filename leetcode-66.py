a50d48156e4ca7373dadb80b8c9639de58113637
craran <graydonaldti@gmail.com>
2024-08-12 17:15:29 +0800

    re-finished leetcode-66.py

--- leetcode-66.py
+++ leetcode-66.py
@@ -2,4 +2,4 @@ from typing import List
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         s = int(str(''.join(list(map(str, digits))))) + 1
-        return [int(x) for x in list(str(s))]
+        return list(map(int, list(str(s))))
