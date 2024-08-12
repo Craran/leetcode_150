85946c6b94ba6d4cbee94b5d85ba804887a69da7
craran <graydonaldti@gmail.com>
2024-08-12 13:57:54 +0800

    finished leetcode-383.py

--- leetcode-383.py
+++ leetcode-383.py
@@ -1,3 +1,18 @@
+from typing import Dict
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        
+        hash_table: Dict[str, int] = {}
+        for s in magazine:
+            if s not in hash_table:
+                hash_table[s] = 1
+            else:
+                hash_table[s] += 1
+
+
+        for s in ransomNote:
+            if s in hash_table and hash_table[s] > 0:
+                hash_table[s] -= 1
+            else:
+                return False
+
+        return True
