a6554f61e30900974e6e332bf57d6284b7ed0ad6
craran <graydonaldti@gmail.com>
2024-08-13 11:32:51 +0800

    finished leetcode-228.py

--- leetcode-228.py
+++ leetcode-228.py
@@ -1,4 +1,28 @@
 from typing import List
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        
+        if not nums:
+            return []
+        i = j = 0
+        result: List[int] = []
+        ans: List[str] = []
+
+        while True:
+            if j + 1 == len(nums):
+                result.append(nums[i: j + 1])
+                break
+            if nums[j + 1] == nums[i] + j - i + 1:
+                j += 1
+            else:
+                result.append(nums[i: j + 1])
+                i = j = j + 1
+
+        def trans(input:List[int]) -> str:
+            if len(input) == 1:
+                return str(input[0])
+            else:
+                return str(input[0]) + '->' + str(input[-1])
+            
+
+        ans = list(map(trans, result))
+        return ans
