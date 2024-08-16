999da3bfe8cfb6e12a5121e3e21dcaafef8a7b81
craran <graydonaldti@gmail.com>
2024-08-16 16:07:57 +0800

    finished leetcode-112.py

--- leetcode-112.py
+++ leetcode-112.py
@@ -5,7 +5,37 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-
+        if root == None:
+            return False
+        sum = root.val
+        flag = False
         
+        def dfs(root: Optional[TreeNode]) -> None:
+            nonlocal sum
+            nonlocal targetSum
+            nonlocal flag
+            if root.left == None and root.right == None:
+                
+                if sum == targetSum:
+                    flag = True
+                return None
+            if root.left != None:
+                sum += root.left.val
+                dfs(root.left)
+                sum -= root.left.val
+            if root.right != None:
+                sum += root.right.val
+                dfs(root.right)
+                sum -= root.right.val
+
+        dfs(root)
+        return flag
+            
+            
+            
+
+
+            
