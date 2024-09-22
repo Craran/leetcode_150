b4b6cdd063667b50b448188d2355921a6e8e610a
craran <graydonaldti@gmail.om>
2024-09-22 14:36:39 +0800

    finished leetcode-97.py

--- leetcode-97.py
+++ leetcode-97.py
@@ -3,48 +3,30 @@ from typing import List
 class Solution:
     # @staticmethod
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        m = n = 0
-        length_1, length_2, length_3 = len(s1), len(s2), len(s3)
-        if length_1 + length_2 != length_3:
+        n1, n2, n3 = len(s1), len(s2), len(s3)
+        if n1 + n2 != n3:
             return False
-        if length_1 == 0:
-            return True if s2 == s3 else False
-        if length_2 == 0:
-            return True if s1 == s3 else False
-        flag: bool = False
-        i = j = k = 0
-
-        while True:
-            
-            
-            if flag == False:
-                while i < length_1 and s1[i] == s3[k]:
-                    i += 1
-                    k += 1
-                m += 1
-                flag = True
-            else:
-                while j < length_2 and s2[j] == s3[k]:
-                    j += 1
-                    k += 1
-                n += 1
-                flag = False
-
-            if k >= length_3:
+        f: List[List[bool]] = [[False for _ in range(205)] for _ in range(205)]
+        f[0][0] = True
+        for j in range(1, n2 + 1):
+            f[0][j] = True if f[0][j - 1] == True and s2[j - 1] == s3[j - 1] else False
+            if f[0][j] == False:
                 break
-            
-            if i == length_1 and j == length_2 and k == length_3:
+
+        for i in range(1, n1 + 1):
+            f[i][0] = True if f[i - 1][0] == True and s1[i - 1] == s3[i - 1] else False
+            if f[i][0] == False:
                 break
-            elif i == length_1 and s2[j] != s3[k] or j == length_2 and s1[i] != s3[k]:
-                return False
-            elif i < length_1 and s1[i] != s3[k] and j < length_2 and s2[j] != s3[k]:
-                return False
-            
-            
-        
-        return True
 
-# a = Solution()
-# print(a.isInterleave("aabcc", "dbbca", "aadbbcbcac"))
+        for i in range(1, n1 + 1):
+            for j in range(1, n2 + 1):
+                f[i][j] = True if f[i - 1][j] == True and s1[i - 1] == s3[i + j - 1] or \
+                    f[i][j - 1] == True and s2[j - 1] == s3[i + j - 1] else False
+                
 
-# print(Solution.isInterleave(None, "aabcc", "dbbca", "aadbbcbcac"))
+        return f[n1][n2]
+
+    
+
+# Solution.isInterleave(None, "aabcc", "dbbca", "aadbbcbcac")
+
