6797f446ec9faab02635685a7e8a1343f9b25f22
craran <graydonaldti@gmail.om>
2024-09-26 18:33:02 +0800

    finished leetcode-19.py

--- leetcode-19.py
+++ leetcode-19.py
@@ -7,5 +7,23 @@ class ListNode:
         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        fast = slow = head
+        step = 0
+        while True:
+            if step <= n:
+                fast = fast.next
+                step += 1
+            else:
+                fast = fast.next
+                slow = slow.next
+                step += 1
 
-        ...
+            if fast == None:
+                if n >= step:
+                    return head.next
+                else:
+                    slow.next = slow.next.next
+                    return head
+
+
+            
