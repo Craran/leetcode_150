41a49a79d25441aeb37470b289894c205e920362
craran <graydonaldti@gmail.om>
2024-09-27 15:10:23 +0800

    finished leetcode-452.py

--- leetcode-452.py
+++ leetcode-452.py
@@ -2,5 +2,18 @@ from typing import List
 
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
+        n = len(points)
+        if n <= 1:
+            return 1
+        points.sort(key=lambda x: x[0])
+        
+        result: List[List[int]] = [points[0]]
+        
+        for i in range(1, n):
+            if result[-1][1] >= points[i][0]:
+                result[-1][0] = max(result[-1][0], points[i][0])
+                result[-1][1] = min(result[-1][1], points[i][1])
+            else:
+                result.append(points[i])
 
-        ...
+        return len(result)
